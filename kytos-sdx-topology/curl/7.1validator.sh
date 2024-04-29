10dbb48f64c740b70f60ce56334de42459367bb3
lmarinve <lmarinve@gmail.com>
2024-04-29 13:01:14 -0400

    removing curl directory

--- kytos-sdx-topology/curl/7.1validator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-API="http://192.168.0.14:8000/validator/v1/validate"
-echo "##### validate sdx topology #####"
-curl -H "Content-Type: application/json" -X POST $API -d '{}'
--- kytos-sdx-topology/curl/get_global_topology.sh
+++
@@ -1,3 +0,0 @@
-curl -X 'GET' \
-  'http://localhost:8080/SDX-Controller/1.0.0/topology' \
-  -H 'accept: text/plain'
